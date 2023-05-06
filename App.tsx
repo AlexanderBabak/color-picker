d8947c915d993c4ff33f5f10c8223ff102dfbeac
Alexander Babak <alexander.babak@lastingdynamics.com>
2023-05-06 15:03:34 +0300

    install reanimated and gesture

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import Animated from 'react-native-reanimated';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Animated.View style={styles.container}>
+        <Text style={styles.text}>Hello</Text>
+      </Animated.View>
+    </GestureHandlerRootView>
   );
 }
 
